db88b0436e25363e7286b5d66c3495ed77125116
StellarRepositories <169072172+StellarRepositories@users.noreply.github.com>
2024-05-06 17:33:26 +0200

    Added full project via upload
    this project is finished, and serves as an example project in the repo. Uploaded all source code.

---
+++ codewars1/codewars1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+// take input
+// go through the input until the end
+// every substring is a morse symbol
+// make the string longer by adding the next substring as a character
+
+
+// take input
+// go through the input until the end
+// every substring is a morse symbol
+// make the string longer by adding the next substring as a character
+string morseCode = ".... . -.--   .--- ..- -.. .";
+string morseLetter = "";
+string result = "";
+int spaceCount = 0;
+int forEachCount = 0;
+foreach (char c in morseCode)
+{
+    morseLetter += c;
+    Console.WriteLine("char c = " + c);
+    forEachCount++;
+    if (c == ' ' && spaceCount != 0)
+    {
+        spaceCount++;
+        if (spaceCount == 3)
+        {
+            result += " ";
+            Console.WriteLine("MorseCode.Get(morseLetter) =  ");
+        }
+        continue;
+    }
+    spaceCount = 0;
+    Console.WriteLine("For Each Count: " + forEachCount + "  morseCode.Length: " + morseCode.Length);
+    if ((c == ' ' && spaceCount == 0) || forEachCount == morseCode.Length)
+    {
+        spaceCount++;
+        result += MorseCode(morseLetter); // should be MorseCode.Get(morseLetter)
+        Console.WriteLine("MorseCode.Get(morseLetter) = " + MorseCode(morseLetter));
+        morseLetter = "";
+        continue;
+    }
+
+
+
+}
+Console.WriteLine(result);
+
+
+
+public static partial class Program
+{
+    public static string MorseCode(string myString)
+    {
+        return "a";
+    }
+
+}
